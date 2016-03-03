423fc0d352375a12fc3bedad71ea78df60de0d5c
Omar Shaikh <oshaikh13@gmail.com>
2016-03-03 10:56:03 +0300

    Fixed BSearch

--- BinarySearch.java
+++ BinarySearch.java
@@ -1,26 +1,29 @@
 class BinarySearch {
     public static void main(String[] args) {
-        int [] x = {1, 2, 3, 4, 5};
+        int [] x = { 0, 2, 4, 6, 8, 10, 12, 14, 16 };
+        
 
-        System.out.println(binarySearch(x, 0, x.length - 1, 4));
+        System.out.println(binarySearch(x, 0, x.length - 1, 16));
     }
 
     private static int binarySearch (int [] elements, int left, int right, int target){
-        if (left == right) {
-            return -1;
-        } 
 
         int mid = (left + right) / 2;
 
+        if (left > right) {
+            return -1;
+        } 
+
         if (elements[mid] == target) {
             return mid;
+        }
 
-        } else if (elements[mid] < target) {
-            left = mid;
+        if (elements[mid] < target) {
+            left = mid + 1;
             return binarySearch(elements, left, right, target);
 
         } else {
-            right = mid;
+            right = mid - 1;
             return binarySearch(elements, left, right, target);
         }
     }
